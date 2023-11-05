5f675dfefc1b623c8d891dbb1155a686bbba133b
Sugeng Budi Arter <sugengbudi12345@gmail.com>
2023-11-05 09:15:27 +0700

    1

---
+++ contracts/drop-point.sol
@@ -0,0 +1,52 @@
+// Import library yang diperlukan
+pragma solidity ^0.8.0;
+
+// Definisikan kontrak SampahContract
+contract SampahContract {
+    // Struktur untuk menyimpan data sementara sebelum dimasukkan ke blockchain
+    struct SampahData {
+        uint idSampah;
+        uint idUser;
+        string jenisSampah;
+        uint berat;
+        string tanggal;
+        string dropPoint;
+    }
+
+    // Penampung data sementara sebelum dimasukkan ke blockchain
+    SampahData[] public sampahPenampung;
+
+    // Fungsi untuk menambahkan data ke penampung
+    function addSampahData(
+        uint _idSampah,
+        uint _idUser,
+        string memory _jenisSampah,
+        uint _berat,
+        string memory _tanggal,
+        string memory _dropPoint
+    ) public {
+        SampahData memory newSampah = SampahData(_idSampah, _idUser, _jenisSampah, _berat, _tanggal, _dropPoint);
+        sampahPenampung.push(newSampah);
+    }
+
+    // Fungsi untuk memvalidasi dan memasukkan data ke dalam blockchain
+    function validateAndAddToBlockchain(uint _index) public {
+        require(_index < sampahPenampung.length, "Indeks tidak valid");
+        SampahData memory dataToValidate = sampahPenampung[_index];
+        // Lakukan validasi data di sini sesuai dengan persyaratan Anda
+        // Jika data valid, Anda dapat menyimpannya di blockchain
+        // Misalnya, Anda dapat menyimpannya di dalam struktur data baru di sini atau melakukan operasi lain yang diperlukan.
+        // Setelah itu, Anda dapat memancarkan peristiwa untuk memberi tahu bahwa data telah dimasukkan.
+        emit SampahDataAdded(dataToValidate.idSampah, dataToValidate.idUser, dataToValidate.jenisSampah, dataToValidate.berat, dataToValidate.tanggal, dataToValidate.dropPoint);
+        delete sampahPenampung[_index]; // Hapus data dari penampung
+    }
+
+    // Fungsi untuk menghapus data dari penampung
+    function clearPenampung(uint _index) public {
+        require(_index < sampahPenampung.length, "Indeks tidak valid");
+        delete sampahPenampung[_index];
+    }
+
+    // Event yang akan memancarkan data yang telah dimasukkan ke dalam blockchain
+    event SampahDataAdded(uint idSampah, uint idUser, string jenisSampah, uint berat, string tanggal, string dropPoint);
+}
