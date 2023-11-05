2ce2197251dbde5963f24f3ea5a7c186b0773d24
Sugeng Budi Arter <sugengbudi12345@gmail.com>
2023-11-05 18:39:16 +0700

    Update drop-point.sol

--- contract/contracts/drop-point.sol
+++ contract/contracts/drop-point.sol
@@ -29,60 +29,39 @@ contract SampahContract {
         sampahPenampung.push(newSampah);
     }
 
-    // Fungsi untuk memvalidasi dan memasukkan data ke dalam blockchain
-    function validateAndAddToBlockchain(uint _index) public {
-        // Pastikan _index sesuai dengan panjang penampung
-        if (_index >= sampahPenampung.length) revert("Indeks tidak valid");
-        
-        SampahData memory dataToValidate = sampahPenampung[_index];
-        // Lakukan validasi data di sini sesuai dengan persyaratan Anda
-        // Jika data valid, Anda dapat menyimpannya di blockchain
-        // Misalnya, Anda dapat menyimpannya di dalam struktur data baru di sini atau melakukan operasi lain yang diperlukan.
-        // Setelah itu, Anda dapat memancarkan peristiwa untuk memberi tahu bahwa data telah dimasukkan.
-        emit SampahDataAdded(dataToValidate.idSampah, dataToValidate.idUser, dataToValidate.jenisSampah, dataToValidate.berat, dataToValidate.tanggal, dataToValidate.dropPoints);
-        delete sampahPenampung[_index]; // Hapus data dari penampung
+    // Fungsi untuk memvalidasi dan memasukkan data ke dalam blockchain berdasarkan ID sampah
+    function validateAndAddToBlockchain(uint _idSampah) public {
+        for (uint i = 0; i < sampahPenampung.length; i++) {
+            if (sampahPenampung[i].idSampah == _idSampah) {
+                SampahData memory dataToValidate = sampahPenampung[i];
+                // Lakukan validasi data di sini sesuai dengan persyaratan Anda
+                // Jika data valid, Anda dapat menyimpannya di blockchain
+                // Misalnya, Anda dapat menyimpannya di dalam struktur data baru di sini atau melakukan operasi lain yang diperlukan.
+                // Setelah itu, Anda dapat memancarkan peristiwa untuk memberi tahu bahwa data telah dimasukkan.
+                emit SampahDataAdded(dataToValidate.idSampah, dataToValidate.idUser, dataToValidate.jenisSampah, dataToValidate.berat, dataToValidate.tanggal, dataToValidate.dropPoints);
+                delete sampahPenampung[i]; // Hapus data dari penampung
+                return;
+            }
+        }
+        revert("Data sampah dengan ID tertentu tidak ditemukan.");
     }
 
     // Fungsi untuk mengirim sampah ke drop point A
-    function sendToDropPointA(uint _index) public {
-        // Pastikan _index sesuai dengan panjang penampung
-        if (_index >= sampahPenampung.length) revert("Indeks tidak valid");
-
-        SampahData storage dataToSend = sampahPenampung[_index];
-        for (uint i = 0; i < dataToSend.dropPoints.length; i++) {
-            if (keccak256(bytes(dataToSend.dropPoints[i])) == keccak256("A")) {
-                // Lakukan logika untuk mengirim sampah ke drop point A di sini
-                // Misalnya, Anda dapat mengupdate status di sini.
+    function sendToDropPointA(uint _idSampah) public {
+        for (uint i = 0; i < sampahPenampung.length; i++) {
+            if (sampahPenampung[i].idSampah == _idSampah) {
+                for (uint j = 0; j < sampahPenampung[i].dropPoints.length; j++) {
+                    if (keccak256(bytes(sampahPenampung[i].dropPoints[j])) == keccak256("A")) {
+                        // Lakukan logika untuk mengirim sampah ke drop point A di sini
+                        // Misalnya, Anda dapat mengupdate status di sini.
+                        sampahPenampung[i].dropPoints.push("B"); // Menambahkan "B" sebagai tujuan tambahan
+                        sampahPenampung[i].dropPoints.push("C"); // Menambahkan "C" sebagai tujuan tambahan
+                        return;
+                    }
+                }
             }
         }
-    }
-
-    // Fungsi untuk mengirim sampah ke drop point B
-    function sendToDropPointB(uint _index) public {
-        // Pastikan _index sesuai dengan panjang penampung
-        if (_index >= sampahPenampung.length) revert("Indeks tidak valid");
-
-        SampahData storage dataToSend = sampahPenampung[_index];
-        for (uint i = 0; i < dataToSend.dropPoints.length; i++) {
-            if (keccak256(bytes(dataToSend.dropPoints[i])) == keccak256("B")) {
-                // Lakukan logika untuk mengirim sampah ke drop point B di sini
-                // Misalnya, Anda dapat mengupdate status di sini.
-            }
-        }
-    }
-
-    // Fungsi untuk mengirim sampah ke drop point C
-    function sendToDropPointC(uint _index) public {
-        // Pastikan _index sesuai dengan panjang penampung
-        if (_index >= sampahPenampung.length) revert("Indeks tidak valid");
-
-        SampahData storage dataToSend = sampahPenampung[_index];
-        for (uint i = 0; i < dataToSend.dropPoints.length; i++) {
-            if (keccak256(bytes(dataToSend.dropPoints[i])) == keccak256("C")) {
-                // Lakukan logika untuk mengirim sampah ke drop point C di sini
-                // Misalnya, Anda dapat mengupdate status di sini.
-            }
-        }
+        revert("Data sampah tidak ditemukan atau bukan tujuan A.");
     }
 
     // Fungsi untuk menghapus data dari penampung
