da29f6ca43e8577c1564de534aafce12e104fe9b
lilbimz_ <lefrandbima@gmail.com>
2023-11-15 13:02:50 +0700

    Create User Input

---
+++ website/wmt-app/src/pages/user/WasteInput.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ThemeSetting from "../components/ThemeSetting";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+
+function WasteInput() {
+  return (
+    <>
+      <div className="container-scroller">
+        {/* partial:../../partials/_navbar.html */}
+        <Navbar />
+        {/* partial */}
+        <div className="container-fluid page-body-wrapper">
+          {/* partial:../../partials/_settings-panel.html */}
+          <ThemeSetting />
+          {/* partial */}
+          {/* partial:../../partials/_sidebar.html */}
+          <Sidebar />
+          {/* partial */}
+          <div className="main-panel">
+            <div className="content-wrapper">
+              <div className="row">
+                <div className="col-12 grid-margin stretch-card">
+                  <div className="card">
+                    <div className="card-body">
+                      <h4 className="card-title">Waste Input</h4>
+                      <p className="card-description">
+                        Input the waste that you want to be dropped off to us
+                      </p>
+                      <form className="forms-sample">
+                        <div className="form-group">
+                          <label htmlFor="exampleInputId1">ID</label>
+                          <input
+                            type="number"
+                            className="form-control"
+                            id="exampleInputId1"
+                            placeholder="Id"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="exampleInputName1">Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputName1"
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="exampleInputEmail3">
+                            Email address
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail3"
+                            placeholder="Email"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="exampleSelectWasteType">Waste Type</label>
+                          <select
+                            className="form-control"
+                            id="exampleSelectWasteType"
+                          >
+                            <option>Plastic</option>
+                            <option>Electronic</option>
+                            <option>Glass</option>
+                            <option>Can</option>
+                            <option>Other</option>
+                          </select>
+                        </div>
+
+                        {/* <div className="form-group">
+                          <label htmlFor="exampleInputWasteType">
+                            Waste Type
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputWasteType"
+                            placeholder="Waste Type"
+                          />
+                        </div> */}
+                        <div className="form-group">
+                          <label htmlFor="exampleInputWasteWeight">
+                            Waste Weight
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputWasteWeight"
+                            placeholder="Waste Weight"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="exampleTextarea1">Textarea</label>
+                          <textarea
+                            className="form-control"
+                            id="exampleTextarea1"
+                            rows={4}
+                            defaultValue={""}
+                          />
+                        </div>
+                        <button type="submit" className="btn btn-info mr-2">
+                          Submit
+                        </button>
+                        <button className="btn btn-light">Cancel</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* content-wrapper ends */}
+            {/* partial:../../partials/_footer.html */}
+            <Footer />
+            {/* partial */}
+          </div>
+          {/* main-panel ends */}
+        </div>
+        {/* page-body-wrapper ends */}
+      </div>
+    </>
+  );
+}
+
+export default WasteInput;
