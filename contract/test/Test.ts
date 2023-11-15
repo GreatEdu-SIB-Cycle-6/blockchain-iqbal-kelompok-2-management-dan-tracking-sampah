4729df0f0059a3c699474c653f5ec471655ce3d9
Sugeng Budi Arter <sugengbudi12345@gmail.com>
2023-11-15 19:32:14 +0700

    change folder

--- contract/test/Test.ts
+++ contract/test/Test.ts
@@ -1,63 +1,122 @@
-import { ethers } from "hardhat";
 import chai from 'chai';
 import { DropPointT } from '../typechain';
+import { ethers } from 'hardhat';
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
 
 const { expect } = chai;
 
-describe("DropPointT", () => {
-  let dropPointT: DropPointT;
+describe('Testing DropPointT', async () => {
+  let Library: DropPointT;
+  let Admin: HardhatEthersSigner;
+  let NotAdmin: HardhatEthersSigner;
 
   beforeEach(async () => {
-    const DropPointTFactory = await ethers.getContractFactory("DropPointT");
-    dropPointT = (await DropPointTFactory.deploy()) as DropPointT;
-    await dropPointT.deployed();
+    try {
+      const accounts = await ethers.getSigners();
+      Admin = accounts[0];
+      NotAdmin = accounts[1];
+
+      Library = await (
+        await ethers.getContractFactory('DropPointT')
+      )
+        .connect(Admin)
+        .deploy();
+      
+    } catch (error) {
+      console.error('Error during setup:', error);
+      throw error;
+    }
   });
 
-  it("should input data to penampungan", async () => {
-    const result = await dropPointT.inputDataPenampungan(
+  it('Admin Input Penampung', async () => {
+    await Library.connect(Admin).inputDataPenampungan(
       1,
-      "User",
-      "user@example.com",
+      "Sugeng",
+      "Sugeng1234@gmail.com",
       "Plastic",
+      5,
+      [1]
+    );
+    //console.log(Admin)
+  });
+
+  it('Bukan Admin Input Penampung', async () => {
+    await Library.connect(NotAdmin).inputDataPenampungan(
+      2,
+      "Bima",
+      "Bima1234@gmail.com",
+      "Bom",
+      20,
+      [1]
+    );
+    //console.log(NotAdmin)
+  });
+
+  it('Admin Ingin Verifikasi Transaksi', async () => {
+    await Library.connect(Admin).inputDataPenampungan(
+      5,
+      "Another",
+      "Another1234@gmail.com",
+      "Bom",
+      20,
+      [1]
+    );
+    await Library.connect(Admin).verifikasiDataSampah(3);
+    //console.log(Admin)
+  });
+
+  it('Jika Bukan Admin Ingin Verifikasi Transaksi', async () => {
+    
+    await expect(
+      Library.connect(NotAdmin).verifikasiDataSampah(2)
+    ).to.be.revertedWith('Anda bukan seorang Admin!');
+    //console.log(NotAdmin)
+  });
+
+  it('Admin Ingin Membersihkan Penampung', async () => {
+    await Library.connect(Admin).inputDataPenampungan(
+      3,
+      "fenti",
+      "fenti1234@gmail.com",
+      "Kaca",
       10,
       [1]
     );
 
-    await result.wait();
-    // Add assertions or checks here
+    await Library.connect(Admin).bersihkanDataSampah(4);
+    //console.log(NotAdmin)
   });
 
-  it("should verify data sampah", async () => {
-    await dropPointT.inputDataPenampungan(
-      1,
-      "User",
-      "user@example.com",
-      "Plastic",
-      10,
+  it('Jika Bukan Admin Ingin Membersihkan Penampung', async () => {
+    await Library.connect(Admin).inputDataPenampungan(
+      4,
+      "rifqi",
+      "rifqi1234@gmail.com",
+      "nuklir",
+      50,
       [1]
     );
 
-    const result = await dropPointT.verifikasiDataSampah(1);
-
-    await result.wait();
-    // Add assertions or checks here
+    await expect(
+      Library.connect(NotAdmin).bersihkanDataSampah(5)
+    ).to.be.revertedWith('Anda bukan seorang Admin!');
+    //console.log(NotAdmin)
   });
 
-  it("should send to next drop point", async () => {
-    await dropPointT.inputDataPenampungan(
-      1,
-      "User",
-      "user@example.com",
-      "Plastic",
-      10,
-      [1]
-    );
-    await dropPointT.verifikasiDataSampah(1);
+  it('Admin Ingin Kirim Ke DropPoint Selanjutnya', async () => {
+    await Library.connect(Admin).kirimKeDropPointSelanjutnya(1);
+    //console.log(Admin)
+  });
 
-    const result = await dropPointT.kirimKeDropPointSelanjutnya(1);
+  it('Jika Bukan Admin Ingin Kirim Ke DropPoint Selanjutnya', async () => {
+    await expect(
+      Library.connect(NotAdmin).kirimKeDropPointSelanjutnya(1)
+    ).to.be.revertedWith('Anda bukan seorang Admin!');
+    //console.log(NotAdmin)
+  });
 
-    await result.wait();
-    // Add assertions or checks here
+  it('Jika Admin dan Bukan Admin ingin melihat Data Sampah', async () => {
+    await Library.connect(Admin).getDataSampahById(1);
+    await Library.connect(NotAdmin).getDataSampahById(1);
   });
 });
