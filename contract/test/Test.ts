450d09dc6ed9e606224642b966b0e177cfdf6a7d
Sugeng Budi Arter <sugengbudi12345@gmail.com>
2023-11-17 17:19:05 +0700

    Update Test

--- contract/test/Test.ts
+++ contract/test/Test.ts
@@ -1,113 +1,229 @@
-import chai from 'chai';
-import { DropPointT } from '../typechain';
 import { ethers } from 'hardhat';
+import { Signer } from 'ethers';
+import { expect } from 'chai';
 import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+import { DropPointT } from '../typechain';
+import { log } from 'console';
 
-const { expect } = chai;
+describe('DropPointT Contract - inputDataPenampungan', function () {
+  let owner: HardhatEthersSigner;
+  let user: HardhatEthersSigner;
+  let dropPointT: DropPointT;
 
-describe('Testing DropPointT', async () => {
-  let Library: DropPointT;
-  let Admin: HardhatEthersSigner;
-  let NotAdmin: HardhatEthersSigner;
-
-  beforeEach(async () => {
+  beforeEach(async function () {
     try {
       const accounts = await ethers.getSigners();
-      Admin = accounts[0];
-      NotAdmin = accounts[1];
-
-      Library = await (
-        await ethers.getContractFactory('DropPointT')
-      )
-        .connect(Admin)
-        .deploy();
-
-        console.log('Admin accessed inputDataPenampungan:', await Admin.getAddress());
-        console.log('NotAdmin accessed inputDataPenampungan:', await NotAdmin.getAddress());
-      
+      owner: accounts[0];
+      user: accounts[1];
+  
+      [owner] = await ethers.getSigners();
+      [user] = await ethers.getSigners();
+  
+      const DropPointT = await ethers.getContractFactory('DropPointT');
+      dropPointT = await DropPointT.deploy();    
     } catch (error) {
-      console.error('Error during setup:', error);
-      throw error;
+      console.log("error");
     }
   });
 
-  it('Admin Input Penampung', async () => {
-    await Library.connect(Admin).inputDataPenampungan(
-      "Sugeng",
-      "Sugeng1234@gmail.com",
-      "Plastic",
-      5,
-      [1]
+  it('Owner Input Transaksi Data Penampung', async function () {
+    const adminAddress = await owner.getAddress();
+
+    // Simulate sample data for input
+    const sampleData = {
+      namaPengguna: 'Admin User',
+      emailPengguna: 'admin@example.com',
+      jenisSampah: 'Plastic',
+      berat: 100,
+      dropPoints: [1], 
+    };
+
+    // Call the function to input data as admin
+    await dropPointT.connect(owner).inputDataPenampungan(
+      sampleData.namaPengguna,
+      sampleData.emailPengguna,
+      sampleData.jenisSampah,
+      sampleData.berat,
+      sampleData.dropPoints
     );
-    //console.log(msg.sender)
   });
 
-  it('Bukan Admin Input Penampung', async () => {
-    await Library.connect(NotAdmin).inputDataPenampungan(
-      "Bima",
-      "Bima1234@gmail.com",
-      "Bom",
-      20,
-      [1]
+  it('Non - Owner Input Transaksi Data Penampung', async function () {
+    const userAddress = await user.getAddress();
+
+    // Simulate sample data for input by a user
+    const sampleData = {
+      namaPengguna: 'Regular User',
+      emailPengguna: 'user@example.com',
+      jenisSampah: 'Glass',
+      berat: 50,
+      dropPoints: [1], 
+    };
+
+    // Call the function to input data as a regular user
+    await dropPointT.connect(user).inputDataPenampungan(
+      sampleData.namaPengguna,
+      sampleData.emailPengguna,
+      sampleData.jenisSampah,
+      sampleData.berat,
+      sampleData.dropPoints
     );
-    
+
   });
 
-  it('Admin Ingin Verifikasi Transaksi', async () => {
-    await Library.connect(NotAdmin).inputDataPenampungan("Fenti", "Fenti1234@gmail.com", "tes", 5,[1]);
-    await Library.connect(Admin).verifikasiDataSampah((3));
+  it('Owner Verifikasi Transaksi', async function () {
+    const adminAddress = await owner.getAddress();
+
+  // Simulate sample data for input by a user
+  const sampleData = {
+    namaPengguna: 'Regular User',
+    emailPengguna: 'user@example.com',
+    jenisSampah: 'Glass',
+    berat: 50,
+    dropPoints: [1],
+  };
+
+  // Call the function to input data as a regular user
+  await dropPointT.inputDataPenampungan(
+    sampleData.namaPengguna,
+    sampleData.emailPengguna,
+    sampleData.jenisSampah,
+    sampleData.berat,
+    sampleData.dropPoints
+  );
+
+  // Get the latest counter value to determine the sampleId
+  const latestCounter = await dropPointT.jumlahsampah();
+
+  // Use the latest counter value as the sampleId
+  const sampleId = Number(latestCounter);
+
+    // Call the function to verify data sampah by owner
+    await dropPointT.connect(owner).verifikasiDataSampah(sampleId);
   });
 
-  it('Jika Bukan Admin Ingin Verifikasi Transaksi', async () => {
-    
-    await expect(
-      Library.connect(NotAdmin).verifikasiDataSampah(2)
-    ).to.be.revertedWith('Anda bukan seorang Admin!');
-    //console.log(NotAdmin)
+  it('Non - Owner Verifikasi Transaksi', async function () {
+    const userAddress = await user.getAddress();
+
+    // Simulate sample data for input by a user
+    const sampleData = {
+      namaPengguna: 'Regular User',
+      emailPengguna: 'user@example.com',
+      jenisSampah: 'Glass',
+      berat: 50,
+      dropPoints: [1],
+    };
+  
+    // Call the function to input data as a regular user
+    await dropPointT.inputDataPenampungan(
+      sampleData.namaPengguna,
+      sampleData.emailPengguna,
+      sampleData.jenisSampah,
+      sampleData.berat,
+      sampleData.dropPoints
+    );
+  
+    // Get the latest counter value to determine the sampleId
+    const latestCounter = await dropPointT.jumlahsampah();
+  
+    // Use the latest counter value as the sampleId
+    const sampleId = Number(latestCounter); 
+    // Try to call the function to verify data sampah with a non-owner account
+    await expect(dropPointT.connect(user).verifikasiDataSampah(sampleId)).to.be.revertedWith(
+      'Ownable: caller is not the owner'
+    );
   });
 
-  // it('Admin Ingin Membersihkan Penampung', async () => {
-  //   await Library.connect(Admin).inputDataPenampungan(
-  //     "fenti",
-  //     "fenti1234@gmail.com",
-  //     "Kaca",
-  //     10,
-  //     [1]
-  //   );
+  it('Owner kirim Ke drop point selanjutnya', async function () {
+    const adminAddress = await owner.getAddress();
 
-  //   await Library.connect(Admin).bersihkanDataSampah(4);
-  //   //console.log(NotAdmin)
-  // });
+    const sampleData = {
+      namaPengguna: 'Admin',
+      emailPengguna: 'admin@example.com',
+      jenisSampah: 'Plastik',
+      berat: 30,
+      dropPoints: [1], 
+    };
+  
+    // Panggil fungsi untuk memasukkan data sebagai admin
+    await dropPointT.connect(owner).inputDataPenampungan(
+      sampleData.namaPengguna,
+      sampleData.emailPengguna,
+      sampleData.jenisSampah,
+      sampleData.berat,
+      sampleData.dropPoints
+    );
+  
+    // Dapatkan nilai counter terbaru untuk menentukan sampleId
+    const latestCounter = await dropPointT.jumlahsampah();
+  
+    // Gunakan nilai counter terbaru sebagai sampleId
+    const sampleId = Number(latestCounter);
 
-  // it('Jika Bukan Admin Ingin Membersihkan Penampung', async () => {
-  //   await Library.connect(Admin).inputDataPenampungan(
-  //     "rifqi",
-  //     "rifqi1234@gmail.com",
-  //     "nuklir",
-  //     50,
-  //     [1]
-  //   );
+    await dropPointT.connect(owner).verifikasiDataSampah(sampleId);
+  
+    // Panggil fungsi untuk mengirim data ke drop point selanjutnya oleh admin
+    await dropPointT.connect(owner).kirimKeDropPointSelanjutnya(sampleId);
 
-  //   await expect(
-  //     Library.connect(NotAdmin).bersihkanDataSampah(5)
-  //   ).to.be.revertedWith('Anda bukan seorang Admin!');
-  //   //console.log(NotAdmin)
-  // });
-
-  it('Admin Ingin Kirim Ke DropPoint Selanjutnya', async () => {
-    await Library.connect(Admin).kirimKeDropPointSelanjutnya(1);
-    //console.log(Admin)
   });
 
-  it('Jika Bukan Admin Ingin Kirim Ke DropPoint Selanjutnya', async () => {
-    await expect(
-      Library.connect(NotAdmin).kirimKeDropPointSelanjutnya(2)
-    ).to.be.revertedWith('Anda bukan seorang Admin!');
-    //console.log(NotAdmin)
+  it('Non - Owner kirim Ke drop point selanjutnya', async function () {
+    const userAddress = await user.getAddress();
+    const adminAddress = await owner.getAddress();
+
+    const dataContoh = {
+      namaPengguna: 'Pengguna Lain',
+      emailPengguna: 'lainnya@example.com',
+      jenisSampah: 'Logam',
+      berat: 20,
+      dropPoints: [1], // Array contoh drop points
+    };
+  
+    // Panggil fungsi untuk memasukkan data sebagai pengguna lain yang bukan admin
+    await dropPointT.inputDataPenampungan(
+      dataContoh.namaPengguna,
+      dataContoh.emailPengguna,
+      dataContoh.jenisSampah,
+      dataContoh.berat,
+      dataContoh.dropPoints
+    );
+  
+    // Dapatkan nilai counter terbaru untuk menentukan sampleId
+    const latestCounter = await dropPointT.jumlahsampah();
+  
+    // Gunakan nilai counter terbaru sebagai sampleId
+    const sampleId = Number(latestCounter);
+    await dropPointT.connect(owner).verifikasiDataSampah(sampleId);
+  
+    // Coba panggil fungsi untuk mengirim data ke drop point selanjutnya oleh pengguna lain yang bukan admin
+    await expect(dropPointT.connect(user).kirimKeDropPointSelanjutnya(sampleId)).to.be.revertedWith(
+      'Ownable: caller is not the owner'
+    );
   });
 
-  it('Jika Admin dan Bukan Admin ingin melihat Data Sampah', async () => {
-    await Library.connect(Admin).getDataSampahById(1);
-    await Library.connect(NotAdmin).getDataSampahById(2);
+  it('Owner Get Data SampahList', async function () {
+    const adminAddress = await owner.getAddress();
+
+        // Dapatkan nilai counter terbaru untuk menentukan sampleId
+        const latestCounter = await dropPointT.jumlahsampah();
+  
+        // Gunakan nilai counter terbaru sebagai sampleId
+        const sampleId = Number(latestCounter);
+
+        await(dropPointT.connect(owner)).getDataSampahById(sampleId);
+
+  });
+
+  it('Non - Owner Get Data SampahList', async function () {
+    const userAddress = await user.getAddress();
+
+        // Dapatkan nilai counter terbaru untuk menentukan sampleId
+        const latestCounter = await dropPointT.jumlahsampah();
+  
+        // Gunakan nilai counter terbaru sebagai sampleId
+        const sampleId = Number(latestCounter);
+
+        await(dropPointT.connect(user)).getDataSampahById(sampleId);
+
   });
 });
