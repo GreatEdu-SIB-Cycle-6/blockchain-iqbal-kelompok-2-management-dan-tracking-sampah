dfd38711ed91c643cbe478a7178dba1c64d22cd4
FENTI SEPTRI <fhentyputryahmad28@gmail.com>
2023-11-19 16:08:46 +0700

    commit
    commit to main

---
+++ contract/contracts/user.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.0;
+
+contract User {
+    uint id;
+    string private nama;
+    string private jenis_kelamin;
+    string private ttl;
+    string agama; 
+} 
