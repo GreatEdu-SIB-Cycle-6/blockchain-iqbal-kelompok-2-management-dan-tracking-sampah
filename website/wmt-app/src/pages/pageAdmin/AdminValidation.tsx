2b390c9f844ac5e3f7b199acbd9796d922e5e574
lilbimz_ <lefrandbima@gmail.com>
2023-11-27 13:21:01 +0700

    Update and add auth for admin validation

--- website/wmt-app/src/pages/pageAdmin/AdminValidation.tsx
+++ website/wmt-app/src/pages/pageAdmin/AdminValidation.tsx
@@ -1,23 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "../components/Navbar";
 import ThemeSetting from "../components/ThemeSetting";
 import AdminSidebar from "../components/AdminSidebar";
 import Footer from "../components/Footer";
-
-import { useAccount, usePrepareContractWrite, useContractWrite } from "wagmi";
+import {
+  useAccount,
+  usePrepareContractWrite,
+  useContractWrite,
+  useContractRead,
+} from "wagmi";
 import DropPoinTAbi from "../../assets/DropPoinTAbi.json";
+import { useNavigate } from "react-router-dom";
 
 function WasteInput() {
+  const contractDropPoinT = "0xfb33CBBe4ea51F3e35EbA76612Ab487C257193a6";
+  const navigate = useNavigate();
+  const { address, isDisconnected } = useAccount();
+
+  const { data: admin } = useContractRead({
+    address: contractDropPoinT,
+    abi: DropPoinTAbi as any,
+    functionName: "owner",
+  });
+  console.log("admin:", admin);
+
+  useEffect(() => {
+    if (isDisconnected === true) {
+      navigate('/alert');
+    }
+  }, [isDisconnected, navigate]);
+
+  useEffect(() => {
+    if (address !== admin) {
+      navigate('/forbidden');
+    }
+  }, [address, admin, navigate]);
+
   const [wasteId, setwasteId] = useState("");
   const handlewasteId = (event: any) => setwasteId(event.target.value);
 
+  const [wasteIdDeleted, setwasteIdDeleted] = useState("");
+  const handlewasteIdDeleted = (event: any) => setwasteIdDeleted(event.target.value);
   
-  const { address } = useAccount();
-
-  const contractDropPoinT = "0x097ecb3C88e5cD27033a816683c28d779a1f7693";
-
   const { config: configWasteInput } = usePrepareContractWrite({
     address: contractDropPoinT,
     abi: DropPoinTAbi as any,
@@ -26,6 +52,21 @@ function WasteInput() {
   });
   const { write: writeWasteId } = useContractWrite(configWasteInput);
 
+  const { config: configWasteDelete } = usePrepareContractWrite({
+    address: contractDropPoinT,
+    abi: DropPoinTAbi as any,
+    functionName: "bersihkanDataSampah",
+    args: [wasteId],
+  });
+  const { write: writeDeleteWasteId } = useContractWrite(configWasteDelete);
+
+  const { data: getWasteData } = useContractRead({
+    address: contractDropPoinT,
+    abi: DropPoinTAbi as any,
+    functionName: "getAllDataPenampungan",
+  });
+  console.log("getWasteData:", getWasteData);
+  console.log("getWasteData result:", getWasteData);
   const showNotification = () => {
     toast.info("Wait Metamask Notification", {
       position: toast.POSITION.TOP_RIGHT,
@@ -33,9 +74,9 @@ function WasteInput() {
   };
 
   const handleSubmit = async () => {
-    showNotification(); 
+    showNotification();
     try {
-      await writeWasteId?.();
+      await writeWasteId ?.();
       toast.success("Validation Success, Wait for Wallet Notification!", {
         position: toast.POSITION.TOP_RIGHT,
       });
@@ -46,6 +87,31 @@ function WasteInput() {
       });
     }
   };
+
+  const handleSubmit2 = async () => {
+    showNotification();
+    try {
+      await writeDeleteWasteId ?.();
+      toast.success("Unvalidation Success, Wait for Wallet Notification!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } catch (error) {
+      console.error("Error during submission:", error);
+      toast.error("Error during submission", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  const [tableData, setTableData] = useState<any[]>([]);
+
+  useEffect(() => {
+    // Update the local state when getWasteData changes
+    if (getWasteData) {
+      setTableData([...getWasteData]);
+    }
+  }, [getWasteData]);
+
 
   return (
     <>
@@ -65,8 +131,58 @@ function WasteInput() {
                         Input the waste id that you want to be validated
                       </p>
                       <form className="forms-sample">
+                      <div className="table-responsive">
+                        <table
+                          className="display expandable-table"
+                          style={{ width: "100%" }}
+                        >
+                          <thead>
+                            <tr>
+                              <th>User Address</th>
+                              <th>Name</th>
+                              <th>Email</th>
+                              <th>Waste ID</th>
+                              <th>Waste Type</th>
+                              <th>Waste Weight</th>
+                              <th>Date</th>
+                              <th>Droppoint</th>
+                              <th>Validation</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {tableData.map((data: any, index: number) =>(
+                                <tr key={index}>
+                                  <td>{data.userAddress}</td>
+                                  <td>{data.namaPengguna}</td>
+                                  <td>{data.emailPengguna}</td>
+                                  <td>{data.idSampah.toString() }</td>
+                                  <td>{data.jenisSampah}</td>
+                                  <td>{data.berat.toString()}</td>
+                                  <td>
+                                    {data.waktu.toString()  &&
+                                      new Date(
+                                        Number(data.waktu.toString()) * 1000
+                                      ).toLocaleString()}
+                                  </td>
+                                  <td>
+          {data.dropPoints && data.dropPoints.length > 0
+            ? data.dropPoints[0].toString()
+            : "No drop points"}
+        </td>
+                                  <td>
+                                    {data.sudahDivalidasi
+                                      ? "Validated"
+                                      : "Not Validated"}
+                                  </td>
+                                </tr>
+                              ))}
+                          </tbody>
+                        </table>
+                      </div>
                         <div className="form-group">
-                          <label htmlFor="exampleInputName1">Waste ID To Validated</label>
+                          <label htmlFor="exampleInputName1">
+                            Waste ID To Validated
+                          </label>
                           <input
                             value={wasteId}
                             onChange={handlewasteId}
@@ -87,6 +203,48 @@ function WasteInput() {
                         </button>
 
                         <button className="btn btn-light">Cancel</button>
+
+                        
+
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12 grid-margin stretch-card">
+                  <div className="card">
+                    <div className="card-body">
+                      <h4 className="card-title">Waste Unvalidation</h4>
+                      <p className="card-description">
+                        Input the waste id that you want to be unvalidated
+                      </p>
+                      <form className="forms-sample">
+                        <div className="form-group">
+                          <label htmlFor="exampleInputName1">
+                            Waste ID To Unvalidated
+                          </label>
+                          <input
+                            value={wasteIdDeleted}
+                            onChange={handlewasteIdDeleted}
+                            type="text"
+                            className="form-control"
+                            id="exampleInputName1"
+                            placeholder="Waste ID"
+                          />
+                        </div>
+                        <ToastContainer />
+
+                        <button
+                          type="button"
+                          className="btn btn-info mr-2"
+                          onClick={handleSubmit2}
+                        >
+                          Unvalidation
+                        </button>
+
+                        <button className="btn btn-light">Cancel</button>
+
+                        
+
                       </form>
                     </div>
                   </div>
